1533eb646f93f9c78faca6b45b8b7b034f3ee38f
Salvesti <olah.brad@gmail.com>
2017-02-01 15:32:11 -0500

    Added .project and .classpath file.

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -10,7 +10,7 @@ public class Runner
 	public static void main(String[] args)
 	{
 		DataHandler funHandler = new DataHandler("fun.dat");
-		funHandler.addItem("John", "11:11", 11);
+		funHandler.addItem("John", "11:11", 21);
 		funHandler.closeFile();
 	}
 
